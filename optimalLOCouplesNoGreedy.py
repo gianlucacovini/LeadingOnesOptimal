53b361b85af189aa34a2bd76944da4b9dc65144e
gianlucacovini <gianlucacovini00@gmail.com>
2024-07-08 18:25:21 +0200

    Update optimalLOCouplesNoGreedy.py

--- optimalLOCouplesNoGreedy.py
+++ optimalLOCouplesNoGreedy.py
@@ -40,14 +40,19 @@ def categorize_bit_strings(n):
     return results_dict
 
 def terms_calculator(args):
-    k, l, n, couples, num_couples, T = args
+    l, n, couples, T, k = args
     
     A = np.zeros((n-l, n-l))
     
-    # Building the A matrix for the system
+    b = np.ones(n-l) 
+    
     for starting_m in range(l, n): # taking values from l to n-1
+    
         if (l, starting_m) in couples:
             current_nodes = np.array(couples[(l, starting_m)])
+            num_couples = len(current_nodes)
+            
+            in_prob = num_couples / 2**n
         
             for arriving_m in range(l, n): # taking values from l to n-1
                 if (l, arriving_m) in couples:
@@ -62,49 +67,81 @@ def terms_calculator(args):
                                 # ocio ai meno
                                 A[starting_m - l, arriving_m - l] -= 1 / (math.comb(n, k) * num_couples) # non sono sicuro di questa formula ma mi fido del me stesso del passato
             
-        A[starting_m - l, starting_m - l] = 1 + np.sum(A[starting_m - l, :]) # bisogna aggiustare i segni??
-                                                            # Poi attenzione alla vecchia storia del fatto che può stare nella coppia ma esserci transizione di stato e quindi probabilità non nulla
-    
-    # Calculating all other probabilties for l+lambda_ so that I can compute the known term b
-    b = np.ones(n-l) # è giusto o è n-1-m?
-    
-    for starting_mu in range(l, n):
-        P = np.zeros((n+1, n+1))
-    
-        for lambda_ in range(l+1, n):
-            
-            for mu in  range(lambda_, n):
-                if (lambda_, mu) in couples:
-                    nodes = np.array(couples[(lambda_, mu)])
-                    distances = np.sum(np.abs(nodes[:, None, :] - current_nodes[None, :, :]), axis=2)
-                    valid_indices = np.where(distances == k)
-                    
-                    if len(valid_indices[0]) > 0:
-                        for i, j in zip(*valid_indices):
-                            node = nodes[i]
-                            if LeadingOnes(tuple(node)) >= l:
-                                P[lambda_, mu] += 1 / (math.comb(n, k) * num_couples)
-                                # proviamo ad aggiornare direttamente b, va'
-                    
-                    b[starting_mu - l] += P[lambda_, mu] * T[lambda_, mu]
-                    
+            A[starting_m - l, starting_m - l] = -np.sum(A[starting_m - l, :]) - 2*A[starting_m - l, starting_m - l]
+                                                            # Poi attenzione alla vecchia storia del fatto che può stare nella coppia ma esserci transizione di stato e quindi probabilità non nulla: è il motivo di questo -2*
+                                                            
+            P = np.zeros((n+1, n+1))
+        
+            for lambda_ in range(l+1, n+1):
+                
+                for mu in  range(lambda_, n+1):
+                    if (lambda_, mu) in couples:
+                        nodes = np.array(couples[(lambda_, mu)])
+                        distances = np.sum(np.abs(nodes[:, None, :] - current_nodes[None, :, :]), axis=2)
+                        valid_indices = np.where(distances == k)
+                        
+                        if len(valid_indices[0]) > 0:
+                            for i, j in zip(*valid_indices):
+                                node = nodes[i]
+                                if LeadingOnes(tuple(node)) >= l:
+                                    P[lambda_, mu] += 1 / (math.comb(n, k) * num_couples)
+                        
+                        b[starting_m - l] += P[lambda_, mu] * T[lambda_, mu]
+                        
+                        A[starting_m - l, starting_m - l] += P[lambda_, mu]
+                                        
     return A, b
         
 def E_calculator(args):
-    k, l, n, couples, num_couples, T = args
+    l, n, couples, T = args
     
-    # La cosa che devo fare adesso è rimettere a posto un po' le variabili. 
-    # Comunque l'idea c'è, base farla ad un certo punto funzionare
+    A_dict = {}
+    b_dict = {}
+    for k in range(1, n-l+1):
+        args_terms = l, n, couples, T, k
+        A, b = terms_calculator(args_terms)
+        A_dict[k] = A
+        b_dict[k] = b
+        
+        
+    # Ora che ho tutte le righe per A e b mi faccio un po' di cicli mappazzone e trovo 
+    # tutte le x e tengo la migliore
     
-    A, b = terms_calculator(args)
+    # funzione combinatoria che mi calcoli tutti le combinazioni di k per l fissato
     
-    x = np.linalg.solve(A, b)
+        # funzione che mi costruisce le A e b corrispondenti a una combinazione di k
+        
+        # calcolo della x
+        
+        # classico: se è minore dell'ottimo, salva se no passa.
+        # OSS: visto che l'ottimo va calcolato come media meglio tenersi a mente anche num_couples in modo da non doverlo calcolare ogni volta
+        # ma alla fine va bene anche così
+        
+    # Generate all possible combinations of n-l sequences of values of k
+    combinations = list(itertools.product(range(1, n-l+1), repeat=n-l))
     
-    T[l] = x
-    
-    return T
+    x_opt_mean = np.inf
+    for comb in combinations:
+        # Build the corresponding A matrix and b vector
+        A = np.vstack([A_dict[k][i] for i, k in enumerate(comb)])
+        b = np.hstack([b_dict[k][i] for i, k in enumerate(comb)])
 
-
+        try:
+            # Solve the linear system Ax = b
+            x = np.linalg.solve(A, b)
+            
+            x_mean = np.mean(x) # DA CORREGGERE: media pesata sul numero di couples
+            
+            if x_mean < x_opt_mean:
+                k_opt = comb
+                x_opt = x
+                x_opt_mean = x_mean
+            
+        except np.linalg.LinAlgError:
+            # If the matrix A is singular, we can't solve it
+            x = None
+        
+    return x_opt, k_opt
 
 def variables_calculator(n, pool):
     K = np.zeros((n+1, n+1))
@@ -121,12 +158,15 @@ def variables_calculator(n, pool):
     in_prob[(n, n)] = 1 / 2**n
     in_prob[(n-1, n-1)] = 1 / 2**n
 
-    for l in reversed(range(1, n-1)):
-        for k in range(1, n-l+1): # Poi da parallelizzare, vedere meglio
-            args = k, l, n, couples, num_couples, T
+    for l in reversed(range(0, n-1)):
+        args = l, n, couples, T
+        
+        x_opt, k_opt = E_calculator(args) # it gives a vector with expected times for 
             
-            T = E_calculator(args) # Dovrebbe restituire un vettore con i tempi attesi per ogni mu per ogni l
-
+        # ok, ora aggiorniamo K e T e il gioco è quasi fatto
+        K[l, l:n] = k_opt
+        T[l, l:n] = x_opt
+            
     K[(0, 0)] = n
     T[(0, 0)] = 1
     in_prob[(0, 0)] = 1 / 2**n
@@ -193,66 +233,12 @@ def process_iteration(n, pool):
         file.write(f"K: {K}\n")
         file.write(f"T: {T}\n")
 
-    plot_2d_matrix(K, n, "K")
-    plot_2d_matrix(T, n, "T")
+    plot_2d_matrix(K, n)
+    plot_2d_matrix(T, n)
     
     return Expected_time
-    
-def variables_calculator_fulltime(n, K):
-    T = {}
-    
-    nodes = sort_bit_strings(generate_bit_strings(n))
-        
-    c = len(nodes)-2 # number of nodes
-    
-    T[tuple(map(int, np.ones(n)))] = 0
-    
-    current_node = nodes[c] # It starts with the node with all ones and one zero at the end
-                              
-    T[current_node] = n
-    
-    while c != 0: # Verifies that the current node is not the all 0 string
-    
-        # update node
-        c -= 1
-        current_node = nodes[c]
-    
-        m = OneMax(current_node)
-        l = LeadingOnes(current_node)
-    
-        if not np.any(current_node):
-            break
-    
-        k = K[(l, m)]
-        P = {}
-        for node in T.keys(): # We look only between the keys we have already looked, since the lexicographic improvement is possible only towards them
-            if sum(list(abs(a - b) for a, b in zip(node, current_node))) == k: # We consider only the strings where we changed exactly k elements 
-                if LeadingOnes(node) > l:
-                    P[node] = 1/math.comb(n, k)
-                elif LeadingOnes(node) == l:
-                    if sum(node) > sum(current_node): 
-                        P[node] = 1/math.comb(n, k)
-            
-        P_current_node = 1 - sum(P.values())
-        
-        # Calculated expected time with given k
-        if P_current_node != 1:
-            E_current = round((1 + sum([P[node]*T[node] for node in P.keys()]))/(1-P_current_node), 3)
-        else:
-            E_current = 1
-        
-        T[current_node] = E_current
-        
-    T[tuple(map(int, np.zeros(n)))] = 1
-    
-    # Calculate the total expected time for the algorithm
-    Expected_time = sum([T[node]/(2**n) for node in T.keys()]) # We don't have the +1 because we are summing over all n, not until n-1
-    
-    print(f"n = {n}, Expected time = {Expected_time} \n")
-    
-    return T, Expected_time
 
 if __name__ == "__main__":
     with multiprocessing.Pool(processes=core_num) as pool:
-        for n in range(3, 4):
+        for n in range(3, 11):
             process_iteration(n, pool)
